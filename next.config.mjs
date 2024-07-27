d5d9711292a02fa4c77ac300cd320947e062835e
Bao Pham <bao.pham@sapotacorp.vn>
2024-07-27 21:09:51 +0700

    delete the env file

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "lay-restaurant.zeabur.app",
+      "images.pexels.com",
+      "res.cloudinary.com",
+      "via.placeholder.com",
+      "media.istockphoto.com",
+      "images.unsplash.com",
+      "png.pngtree.com",
+      "img.freepik.com",
+      "noithattugia.com",
+      "hips.hearstapps.com",
+    ],
+  },
+};
+
+export default nextConfig;
